9020db549952ae7005ffa73572dfe594099320e9
Seid <58677233+SEID767@users.noreply.github.com>
2020-01-04 13:56:04 -0800

    For loop is added

--- Helloworld.java
+++ Helloworld.java
@@ -4,6 +4,8 @@ public class Helloworld {
 		System.out.println("Hello world");
 		System.out.println("Sedosh");
 		System.out.println("************");
+		For (int i; i<6;i++)
+			System.out.println(" *********** Great ******");
 		System.out.println(" you need to prepare for hajj");
 	}
 
