5e2b3587be078bd4ff6546b90752d22ffe421663
Seid <58677233+SEID767@users.noreply.github.com>
2020-01-04 13:42:48 -0800

    New file created

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Sedosh");
+		System.out.println("************");
+		System.out.println(" you need to prepare for hajj");
+	}
+
+}
